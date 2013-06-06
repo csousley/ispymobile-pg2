e7bae84916c1b7727c24699cb7c0dbfa3ac1df6b
Cy Sousley <cy@cyzer.com>
2013-06-06 16:48:22 -0400

    rework 4

--- js/index.js
+++ js/index.js
@@ -1,168 +1,120 @@
 var pushNotification = null;
+var token = null;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Application Constructor
-    function initialize() {
-        bindEvents();
+// Application Constructor
+function initialize() {
+    bindEvents();
+}
+// Bind any events that are required on startup. Common events are:
+// 'load', 'deviceready', 'offline', and 'online'.
+function bindEvents() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
+// deviceready Event Handler
+//
+// The scope of 'this' is the event. In order to call the 'receivedEvent'
+// function, we must explicity call 'app.receivedEvent(...);'
+function onDeviceReady() {
+    receivedEvent('deviceready');
+    iSpySetup();
+}
+function tokenHandler(msg) {
+    alert("Token Handler " + msg);
+    token = msg;
+    setDeviceID(msg);
+}
+function errorHandler(error) {
+    console.log("Error Handler  " + error);
+    alert(error);
+}
+// result contains any message sent from the plugin call
+function successHandler(result) {
+    alert('Success! Result = '+result);
+}
+function unregister() {
+    try {
+    console.log("unreg starting");
+    //var pushNotification = window.plugins.pushNotification;
+    pushNotification.unregister(
+            function(data){
+                console.log("unreg ok: " + data);
+            },
+            function(data){
+                console.log("unreg bad: " + data);
+            });
+    } catch(e) {
+        console.log("unreg error: " + e);
     }
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    function bindEvents() {
-        document.addEventListener('deviceready', onDeviceReady, false);
+    console.log("unreg finished?");
+}
+// Update DOM on a Received Event
+function receivedEvent(id) {
+    pushNotification = window.plugins.pushNotification;
+    if (isAndroid()) {
+        pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"648816449509","ecb":"app.onNotificationGCM"});
     }
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    function onDeviceReady() {
-        receivedEvent('deviceready');
-        iSpySetup();
+    else {
+        pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
     }
-    function tokenHandler(msg) {
-        alert("Token Handler " + msg);
-        setDeviceID(msg);
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+    setTimeout(function() {
+        $("#deviceID").html("Token Test: " + token);
+    }, 2000);
+}
+// iOS
+function onNotificationAPN(event) {
+    //var pushNotification = window.plugins.pushNotification;
+    console.log("Received a notification! " + event.alert);
+    console.log("event sound " + event.sound);
+    console.log("event badge " + event.badge);
+    console.log("event " + event);
+    if (event.alert) {
+        navigator.notification.alert(event.alert);
     }
-    function errorHandler(error) {
-        console.log("Error Handler  " + error);
-        alert(error);
+    if (event.badge) {
+        console.log("Set badge on  " + pushNotification);
+        pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
     }
-    // result contains any message sent from the plugin call
-    function successHandler(result) {
-        alert('Success! Result = '+result);
+    if (event.sound) {
+        var snd = new Media(event.sound);
+        snd.play();
     }
-    function unregister() {
-        try {
-        console.log("unreg starting");
-        //var pushNotification = window.plugins.pushNotification;
-        pushNotification.unregister(
-                function(data){
-                    console.log("unreg ok: " + data);
-                },
-                function(data){
-                    console.log("unreg bad: " + data);
-                });
-        } catch(e) {
-            console.log("unreg error: " + e);
-        }
-        console.log("unreg finished?");
+}
+// Android
+function onNotificationGCM(e) {
+    switch( e.event )
+    {
+        case 'registered':
+            if ( e.regid.length > 0 )
+            {
+                // Your GCM push server needs to know the regID before it can push to this device
+                // here is where you might want to send it the regID for later use.
+                alert('registration id = '+e.regid);
+            }
+        break;
+
+        case 'message':
+          // this is the actual push notification. its format depends on the data model
+          // of the intermediary push server which must also be reflected in GCMIntentService.java
+          alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+        break;
+
+        case 'error':
+          alert('GCM error = '+e.msg);
+        break;
+
+        default:
+          alert('An unknown GCM event has occurred');
+          break;
     }
-    // Update DOM on a Received Event
-    function receivedEvent(id) {
-        pushNotification = window.plugins.pushNotification;
-        if (isAndroid()) {
-            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"648816449509","ecb":"app.onNotificationGCM"});
-        }
-        else {
-            pushNotification.register(this.tokenHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
-        }
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-    // iOS
-    function onNotificationAPN(event) {
-        //var pushNotification = window.plugins.pushNotification;
-        console.log("Received a notification! " + event.alert);
-        console.log("event sound " + event.sound);
-        console.log("event badge " + event.badge);
-        console.log("event " + event);
-        if (event.alert) {
-            navigator.notification.alert(event.alert);
-        }
-        if (event.badge) {
-            console.log("Set badge on  " + pushNotification);
-            pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
-        }
-        if (event.sound) {
-            var snd = new Media(event.sound);
-            snd.play();
-        }
-    }
-    // Android
-    function onNotificationGCM(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    // Your GCM push server needs to know the regID before it can push to this device
-                    // here is where you might want to send it the regID for later use.
-                    alert('registration id = '+e.regid);
-                }
-            break;
-
-            case 'message':
-              // this is the actual push notification. its format depends on the data model
-              // of the intermediary push server which must also be reflected in GCMIntentService.java
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
-
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
-
-            default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
@@ -187,7 +139,6 @@ function setDeviceID(id) {
     if (isAndroid())
         keyname = "regid";
     window.localStorage.setItem(keyname, id);
-    $("#deviceID").html(keyname + ": " + id);
     
     // window.localStorage.setItem("key", "value");
     // var keyname = window.localStorage.key(i);
