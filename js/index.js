b885b690e7d55ef60e6b86ed20f5e5167eb8f552
Cy Sousley <cy@cyzer.com>
2013-06-06 12:30:06 -0400

    adding icons and starting design

--- js/index.js
+++ js/index.js
@@ -1,28 +1,9 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
+        updateStatus("iSpy Rocks...");
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
@@ -44,13 +25,13 @@ var app = {
     },
     // result contains any message sent from the plugin call
     successHandler: function(result) {
-        alert('Success! Result = '+result)
+        alert('Success! Result = '+result);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var pushNotification = window.plugins.pushNotification;
         // TODO: Enter your own GCM Sender ID in the register call for Android
-        if (device.platform == 'android' || device.platform == 'Android') {
+        if (isAndroid()) {
             pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"648816449509","ecb":"app.onNotificationGCM"});
         }
         else {
@@ -112,5 +93,21 @@ var app = {
               break;
         }
     }
-
 };
+
+function updateStatus(newStatus) {
+    $("#deviceStatus").html(newStatus);
+    console.log("SU: " + newStatus);
+}
+
+function isAndroid() {
+    if (device.platform == 'android' || device.platform == 'Android')
+        return true;
+    return false;
+}
+
+function isIOS() {
+    if (!isAndroid())
+        return true;
+    return false;
+}
