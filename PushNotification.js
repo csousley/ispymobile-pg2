04493ec76e897d1ca6cb1a82c6fe79ed44d716f0
Cy Sousley <cy@cyzer.com>
2013-06-07 10:45:01 -0400

    one more quick fix (fingers crossed)

--- PushNotification.js
+++ PushNotification.js
@@ -17,7 +17,6 @@ PushNotification.prototype.register = function(successCallback, errorCallback, o
         return
     }
     $("#ol").append("<li>REG CONT...</li>");
-    if (cordova == null) { console.log("Cordova is null"); $("#ol").append("<li>Cordova is null...</li>"); }
     cordova.exec(successCallback, errorCallback, "PushPlugin", "register", [options]);
 };
 
