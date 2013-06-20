6169b8cbfc866064ed9d82e26e7b9e9e12562143
Cy Sousley <cy@cyzer.com>
2013-06-20 18:09:38 -0400

    troubleshoot reload

--- js/index.js
+++ js/index.js
@@ -360,7 +360,7 @@ function isAndroid() {
     if (!uCheck(deviceType)) {
         log("step");
         if (uCheck(device) && uCheck(device.platform)) {
-            log("step");
+            log("double step");
             deviceType = device.platform;
             window.localStorage.setItem("devicetype", deviceType);
             log("device platform saved: " + deviceType);
